14f28c8828cc0322db9e0df99499b24a3421a9f5
Joeumar Souza <sjoeumar@lenovo.com>
2019-03-19 16:08:14 -0300

    Adding swiper to deck view

--- components/Card.js
+++ components/Card.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
 import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 
 class Card extends Component {
   render() {
     return (
       <View>
-        <Text>QUESTION</Text>
-        <TouchableOpacity onPress={() => alert('YES')}>
-          <Text>YES</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => alert('NO')}>
-          <Text>NO</Text>
-        </TouchableOpacity>
+        <Text>{this.props.item.question}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity onPress={() => alert('YES')}>
+            <FontAwesome name='check-circle' size={30} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert('NO')}>
+            <FontAwesome name='times-circle' size={30} />
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    alignItems: 'center',
+    textAlign: 'center'
+  }
+});
 
 export default Card;
--- components/Deck.js
+++ components/Deck.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { View, Text, Button, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import Card from './Card';
+import Swiper from 'react-native-swiper';
+
+const cardsData = [
+  {key: 1, question: 'Is 1 a letter?', answer: 'no'},
+  {key: 2, question: 'Is B a letter?', answer: 'yes'},
+  {key: 3, question: 'Is 3 a letter?', answer: 'no'},
+  {key: 4, question: 'Is D a letter?', answer: 'yes'},
+  {key: 5, question: 'Is 5 a letter?', answer: 'no'}
+]
 
 class Deck extends Component {
-  
+  state = {
+    data: cardsData
+  }
+
   static navigationOptions = ({ navigation }) => ({
     headerRight: (
-      <Button
-      onPress={() => navigation.navigate('NewCard')}
-        title="+ Card"
-        color="#000"
-      />
+      <TouchableOpacity onPress={() => navigation.navigate('NewCard')}>
+        <FontAwesome name='comment' size={30} />
+      </TouchableOpacity>
     )
   });
 
   render() {
     return (
-      <View>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Card')}>
-          <Text>Question 1</Text>
-          <Card />
-          <Text>Question 2</Text>
-          <Card />
-          <Text>Question 3</Text>
-          <Card />
-        </TouchableOpacity>
-      </View>
+      <Swiper showsButtons={true}>
+        {this.state.data.map((card) => {
+          return (
+            <View key={card.key}>
+              <Card item={card} />
+            </View>
+          )
+        })}
+      </Swiper>
     )
   }
 }
