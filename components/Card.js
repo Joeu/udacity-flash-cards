e275ccd57268422b70a1c64b57766bb527295e77
Joeumar Souza <sjoeumar@lenovo.com>
2019-03-21 15:54:59 -0300

    Styling question cards

--- components/Card.js
+++ components/Card.js
@@ -3,16 +3,34 @@ import { FontAwesome, Entypo } from '@expo/vector-icons';
 import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 
 class Card extends Component {
+
+  state = {
+    cardBGColor: '#ebeef0'
+  }
+
+  _checkIfAnswerIsCorrect = (answer) => {
+    const _correct = this.props.item.answer.toLowerCase() === answer.toLowerCase()
+      ? '#3CB371'
+      : '#D2691E';
+    this.setState({cardBGColor: _correct});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.questionText}>{this.props.item.question}</Text>
-        <View style={{flexDirection: 'row'}}>
-          <TouchableOpacity onPress={() => alert('YES')}>
-            <FontAwesome name='check-circle' size={30} />
+      <View style={[styles.container, {backgroundColor: this.state.cardBGColor}]}>
+        <View style={styles.questionBox}>
+          <Text style={styles.questionText}>{this.props.item.question}</Text>
+        </View>
+        <View style={styles.answerBox}>
+          <TouchableOpacity 
+            style={[styles.box, {flex: 1}]} 
+            onPress={() => this._checkIfAnswerIsCorrect('yes')}>
+            <FontAwesome style={styles.answerText} name='check-circle' />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => alert('NO')}>
-            <FontAwesome name='times-circle' size={30} />
+          <TouchableOpacity 
+            style={[styles.box, {flex: 1}]} 
+            onPress={() => this._checkIfAnswerIsCorrect('no')}>
+            <FontAwesome style={styles.answerText} name='times-circle' />
           </TouchableOpacity>
         </View>
       </View>
@@ -25,14 +43,33 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginLeft: 25,
+    marginRight: 25,
+    marginTop: 80,
+    marginBottom: 80
   },
-  row: {
+  questionBox: {
     flex: 1,
     alignItems: 'center',
-    textAlign: 'center'
+    justifyContent: 'center',
+  },
+  answerBox: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    height: 50,
+    margin: 10,
   },
   questionText: {
     fontSize: 30,
+    textAlign: 'center'
+  },
+  answerText: {
+    fontSize: 55,
+    justifyContent: 'center',
     textAlign: 'center'
   }
 });
